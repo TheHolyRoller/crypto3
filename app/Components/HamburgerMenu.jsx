1dec5c94b755515b234505987f5dc44640eec5a0
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2024-04-16 16:27:42 +0200

    Components for Crypto Project

---
+++ app/Components/HamburgerMenu.jsx
@@ -0,0 +1,73 @@
+
+
+import "../Styles/HamburgerMenu.css";
+
+
+function HamburgerMenu() {
+
+return (
+<>
+    <aside id='hamburgerMenuContainer' >
+    <label className="hamburgerMenu">
+
+<input type="checkbox" />
+
+</label>
+
+<article className="sidebar" id="hamburgerMenuContainer">
+
+<nav id="hamburgerMenu">
+
+{/* Add in the Hamburger Cards here */}
+
+<div id='hamburgerListContainer'>
+
+
+
+<ul id='hamburgerList' >
+
+<li id='hamburgerListItem'>
+
+Telegram 
+
+
+</li>
+<li id='hamburgerListItem'>
+
+Twitter 
+
+
+</li>
+<li id='hamburgerListItem'>
+
+Dexcreener 
+
+
+</li>
+<li id='hamburgerListItem'>
+
+DexTools 
+
+
+</li>
+<li id='hamburgerListItem'>
+
+Farcaster 
+
+
+
+</li>
+
+</ul>
+
+</div>
+
+</nav>
+
+</article>
+    </aside>
+    </>
+    );
+}
+
+export default HamburgerMenu
--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
+
+
 import Image from "next/image";
 
 import Navbar from '../app/Components/Navbar'; 
+import Hamburger from  "../app/Components/HamburgerMenu"; 
 
 
 export default function Home() {
   return (
     <main id='mainIndexPageContainer' >
 
-    <Navbar/> 
+    {/* <Navbar/>  */}
+    <Hamburger/>
+    
     
    
     </main>
