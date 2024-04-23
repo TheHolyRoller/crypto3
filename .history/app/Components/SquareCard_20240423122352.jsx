d261a3bcf8845d4339dbe491c1478530a1be8876
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2024-04-23 12:26:15 -0700

    Color Palette Overhaul

---
+++ .history/app/Components/SquareCard_20240423122352.jsx
@@ -0,0 +1,45 @@
+
+import '../Styles/SquareCard.css'; 
+
+function SquareCard() {
+  return (
+    <div id='squareCardContainer' >
+    
+    <div id='squareCardBackgroundImageContainer' >
+    
+    <div id='squareCardBackgroundImage'>
+    </div>
+    
+    
+    </div>
+    
+    
+    {/* Add in the info container here */}
+    
+    <div id='squareCardInfoContainer'>
+    
+    <div id='squareCardHeadline' >
+    Bridge & swap your tokens in-app
+    
+    
+    </div>
+    
+    
+    <div id='squareCardExtraInfo'>
+    Swap or move tokens between chains
+    
+    
+    </div>
+    
+    {/* Add in the learn more button here */}
+    
+    
+    </div>
+    
+    
+    </div>
+  );
+  
+}
+
+export default SquareCard
---
+++ .history/app/Components/SquareCard_20240423122502.jsx
@@ -0,0 +1,48 @@
+
+import '../Styles/SquareCard.css'; 
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+
+
+
+function SquareCard() {
+  return (
+    <div id='squareCardContainer' >
+    
+    <div id='squareCardBackgroundImageContainer' >
+    
+    <div id='squareCardBackgroundImage'>
+    </div>
+    
+    
+    </div>
+    
+    
+    {/* Add in the info container here */}
+    
+    <div id='squareCardInfoContainer'>
+    
+    <div id='squareCardHeadline' >
+    Bridge & swap your tokens in-app
+    
+    
+    </div>
+    
+    
+    <div id='squareCardExtraInfo'>
+    Swap or move tokens between chains
+    
+    
+    </div>
+    
+    {/* Add in the learn more button here */}
+    
+    
+    </div>
+    
+    
+    </div>
+  );
+  
+}
+
+export default SquareCard
